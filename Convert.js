3aaea01e077af9abcaacb57b43eb8d850e4760ee
Murphy <murphyyip49@gmail.com>
2019-05-16 20:10:12 -0400

    Added conversion abilities

--- Convert.js
+++ Convert.js
@@ -1,30 +1,22 @@
-use strict;
+"use strict";
 
 const decimal = 10;
 const hex = 16;
 const binary = 2;
 
 class Num {
-	// fields
-	base = 10;
-	digits = '';
-
-	//cons
-	constructor(base, digits) {
-		this.base = base;
-		this.digits = digits;
+	//base = number, digits = string
+	constructor(base=10, digits='0') {
+		this._base = base;
+		this._digits = digits;
 	}
-	//get
-	get base() { return this.base; }
-	get digits() { return this.digits; }
-	//set 
-	set base(b) {
-		this.base = b;
-	}
-	set digits(d) {
-		this.digits = d;
-	}
+	//get set
+	get base() { return this._base; }
+	get digits() { return this._digits; }
+	set base(b) { this._base = b; }
+	set digits(d) { this._digits = d; }
 	//methods
+	/*
 	convert(new_base) {
 		if(this.base == new_base) {
 			return this.digits;
@@ -34,4 +26,106 @@ class Num {
 			}
 		}
 	}
+	*/
 }
+
+//returns actual integer value of a digit, e.g A = 10, F = 15
+//we assume maximum is base 16
+//digit is assumed to be a string (char)
+function toNumValue(digit) {
+	switch (digit) {
+		case 'A':
+			return '10';
+			break;
+		case 'B':
+			return '11';
+			break;
+		case 'C':
+			return '12';
+			break;
+		case 'D':
+			return '13';
+			break;
+		case 'E':
+			return '14';
+			break;
+		case 'F':
+			return '15';
+			break;
+	}
+	return digit;
+}
+
+function toDigitValue(n) {
+	switch (n) {
+		case 10:
+			return 'A';
+			break;
+		case 11:
+			return 'B';
+			break;
+		case 12:
+			return 'C';
+			break;
+		case 13:
+			return 'D';
+			break;
+		case 14:
+			return 'E';
+			break;
+		case 15:
+			return 'F';
+			break;
+	}
+	return String(n);
+}
+
+function toDecimal(num) {
+	try {
+		// checks to make sure input is a Num
+		if (!(num instanceof Num)) throw "Incorrect type given. Expected: Num";
+
+		//converts
+		let res = 0;
+		for (let i = 0; i < num.digits.length; ++i ) {
+			//            base ^ Right-Most-Digit-index * Right-Most-Digit
+			res = res + (num.base ** i) * toNumValue(num.digits.charAt(num.digits.length - (i + 1))); 
+			console.log(`sum at digit [${i+1}]: ${res}, digit: ${num.digits.charAt(num.digits.length - (i + 1))}`)
+		}
+		return res;
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
+
+function toBase(b,num) {
+	try {
+		// checks to make sure input is a Num, and b is a integer
+		if (!(num instanceof Num)) throw "Incorrect type given. Expected: Num";
+		if (typeof(b) !== 'number' || (b % 1 !== 0)) throw "Incorrect type given. Expected: integer";
+		if (b <= 1 || b > 16) throw "Invalid base range";
+
+		//convert num to base 10 first
+		let q = toDecimal(num); //quotient
+		let r = 0; //remainder
+		let res = '';
+		while(true) {
+			console.log(`${q} ..${r}`);
+			r = q % b; 
+			q =  Math.floor(q/b);
+			res = toDigitValue(r) + res; //concats r onto res
+			if(q <= 0) break;
+		}
+		return res;
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
+
+let num1 = new Num(16,'F0');
+let n = 1;
+console.log(num1);
+console.log(toDecimal(num1));
+console.log(toBase(2,num1));
